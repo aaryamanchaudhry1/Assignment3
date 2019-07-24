cce442462a4ccadb81705d19a17027ad2e422cd3
aaryamanchaudhry1 <52710376+aaryamanchaudhry1@users.noreply.github.com>
2019-07-23 23:45:51 -0400

    Add files via upload

--- assets/data/myBooks.ts
+++ assets/data/myBooks.ts
@@ -5,24 +5,24 @@ export const MYBOOKS: Book[]=
         bauthor: 'Franklin W.Dixon',
         bbookgenre: 'Mystery',
         byearpublished: '1931',
-        bpitcure: '../../assets/images/hardy.jpg'},
+        bpitcure: '../assets/images/hardy.jpg'},
 
         {bbookname: 'Five Run Away Together',
         bauthor: 'Enid Blyton',
         bbookgenre: 'Mystery',
         byearpublished: '1944',
-        bpitcure: '../../assets/images/famousfive.jpg'},
+        bpitcure: 'assets/images/famousfive.jpg'},
     
         {bbookname: 'Diary of a Wimpy Kid',
         bauthor: 'Jeff Kinney',
         bbookgenre: 'Humour',
         byearpublished: '2007',
-        bpitcure: '../../assets/images/diary.jpg'},
+        bpitcure: 'assets/images/diary.jpg'},
 
         {bbookname: 'Fun for the Secret Seven',
         bauthor: 'Enid Blyton',
         bbookgenre: 'Mystery',
         byearpublished: '1963',
-        bpitcure: '../../assets/images/secretseven.jpg'}
+        bpitcure: 'assets/images/secretseven.jpg'}
     
 ]
