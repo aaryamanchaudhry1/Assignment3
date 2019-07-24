d6a4d5c3f77923760f6fca4fd9c21ffa49f82882
aaryamanchaudhry1 <52710376+aaryamanchaudhry1@users.noreply.github.com>
2019-07-23 23:40:56 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,28 @@
+import {Book} from '../../app/Book';
+export const MYBOOKS: Book[]=
+[
+    {  bbookname: 'What Happened At Midnight',
+        bauthor: 'Franklin W.Dixon',
+        bbookgenre: 'Mystery',
+        byearpublished: '1931',
+        bpitcure: '../../assets/images/hardy.jpg'},
+
+        {bbookname: 'Five Run Away Together',
+        bauthor: 'Enid Blyton',
+        bbookgenre: 'Mystery',
+        byearpublished: '1944',
+        bpitcure: '../../assets/images/famousfive.jpg'},
+    
+        {bbookname: 'Diary of a Wimpy Kid',
+        bauthor: 'Jeff Kinney',
+        bbookgenre: 'Humour',
+        byearpublished: '2007',
+        bpitcure: '../../assets/images/diary.jpg'},
+
+        {bbookname: 'Fun for the Secret Seven',
+        bauthor: 'Enid Blyton',
+        bbookgenre: 'Mystery',
+        byearpublished: '1963',
+        bpitcure: '../../assets/images/secretseven.jpg'}
+    
+]
